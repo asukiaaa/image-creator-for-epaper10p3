7905c47d7f462dc214cd7be11430778ed41d1b9f
Asuk Kono <asukiaaa@gmail.com>
2019-10-01 19:02:29 +0900

    Apply larger bold font to title

--- main.py
+++ main.py
@@ -7,27 +7,29 @@ from dateutil import parser
 
 pygame.init()
 
-# fo = pygame.font.get_fonts()
-# print(fo)
+fo = pygame.font.get_fonts()
+print(fo)
 
 BLACK = (  0,   0,   0)
 WHITE = (255, 255, 255)
 
 size = [1872,1404]
 canvas = pygame.Surface(size)
-sysfont = pygame.font.SysFont('notosansmonocjksc', 80)
+fontM = pygame.font.SysFont('notosansmonocjksc', 80)
+fontL = pygame.font.SysFont('notosansmonocjksc', 120, bold=True)
 
 canvas.fill(WHITE)
 
-helloText = sysfont.render(u'HaLakeイベント情報', False, BLACK)
+helloText = fontM.render(u'HaLakeイベント情報', False, BLACK)
 canvas.blit(helloText, (0, 0))
 
 CONNPASS_SERIES_ID = 1382 # HaLake
 JP_WEEK_DAYS = [u'日', u'月', u'火', u'水', u'木', u'金', u'土']
 
-sectionYStep = 50
-wordYStep = 100
-currentCursor = wordYStep + sectionYStep
+yStepSection = 50
+yStepFontM = 100
+yStepFontL = 150
+currentCursor = yStepFontM + yStepSection
 
 events = cef.get_events(CONNPASS_SERIES_ID)
 events = cef.remove_past_events(events)
@@ -41,9 +43,9 @@ def get_datetime_str(target_datetime):
     return date_str + week_day_str + time_str
 
 for event in events:
-    titleBlock = sysfont.render(event['title'], False, BLACK)
+    titleBlock = fontL.render(event['title'], False, BLACK)
     canvas.blit(titleBlock, (0, currentCursor))
-    currentCursor += wordYStep
+    currentCursor += yStepFontL
 
     started_at = parser.parse(event['started_at'])
     started_at_str = get_datetime_str(started_at)
@@ -51,13 +53,13 @@ for event in events:
     if 'accepted' in event and event['accepted'] != 0:
         accepted_str = '  ' + str(event['accepted']) + u'人参加予定'
     infoText = started_at_str + accepted_str
-    infoBlock = sysfont.render(infoText, False, BLACK)
+    infoBlock = fontM.render(infoText, False, BLACK)
     canvas.blit(infoBlock, (0, currentCursor))
-    currentCursor += wordYStep + sectionYStep
+    currentCursor += yStepFontM + yStepSection
 
 jpNow = datetime.now(pytz.timezone('Asia/Tokyo'))
 currentTimeStr = get_datetime_str(jpNow)
-updatedTimeText = sysfont.render(currentTimeStr + u'更新', False, BLACK)
+updatedTimeText = fontM.render(currentTimeStr + u'更新', False, BLACK)
 canvas.blit(updatedTimeText, (0, currentCursor))
 
 canvas = pygame.transform.rotate(canvas, 180)
